7f628ec167969760a73aaeb2350d817d6de5e609
Denis Charles <54062367+DenisC-mecen@users.noreply.github.com>
2019-11-15 17:44:05 +0100

    Add files via upload

---
+++ Nettoyage.py
@@ -0,0 +1,8 @@
+import pandas as pd
+
+'Fonction qui recode'
+def recodage(col, codeDict):
+    colCoded = pd.Series(col, copy=True)
+    for key, value in codeDict.items():
+        colCoded.replace(key, value, inplace=True)
+    return colCoded
